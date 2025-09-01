ae16c87932f4c847b81e370652353cf13ade6013
Prason <prasonkc07@gmail.com>
2025-09-01 19:39:26 +0545

    Finished UI Design

--- password-manager/src/components/PasswordGen.jsx
+++ password-manager/src/components/PasswordGen.jsx
@@ -7,17 +7,30 @@ const PasswordGen = () => {
         Password Generator
       </h1>
 
-      <div className="flex justify-around">
-        <div className="w-1/2 m-auto text-center text-slate-100 bg-slate-800 border-slate-700 border-1 rounded-xl h-10 flex items-center justify-center">
-          Password
-        </div>
-        <div>
-          <img src="./" alt="image" />
+      <div className="flex">
+        <div className="w-1/2 m-auto text-center text-slate-100 bg-slate-800 border-slate-700 border-1 rounded-xl h-10 flex items-center justify-around">
+          <input type="text" placeholder="Password" className="p-3" disabled/>
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="cursor-pointer h-6"
+          >
+            <path
+              d="M8 16V18.8C8 19.9201 8 20.4802 8.21799 20.908C8.40973 21.2843 8.71569 21.5903 9.09202 21.782C9.51984 22 10.0799 22 11.2 22H18.8C19.9201 22 20.4802 22 20.908 21.782C21.2843 21.5903 21.5903 21.2843 21.782 20.908C22 20.4802 22 19.9201 22 18.8V11.2C22 10.0799 22 9.51984 21.782 9.09202C21.5903 8.71569 21.2843 8.40973 20.908 8.21799C20.4802 8 19.9201 8 18.8 8H16M5.2 16H12.8C13.9201 16 14.4802 16 14.908 15.782C15.2843 15.5903 15.5903 15.2843 15.782 14.908C16 14.4802 16 13.9201 16 12.8V5.2C16 4.0799 16 3.51984 15.782 3.09202C15.5903 2.71569 15.2843 2.40973 14.908 2.21799C14.4802 2 13.9201 2 12.8 2H5.2C4.0799 2 3.51984 2 3.09202 2.21799C2.71569 2.40973 2.40973 2.71569 2.21799 3.09202C2 3.51984 2 4.07989 2 5.2V12.8C2 13.9201 2 14.4802 2.21799 14.908C2.40973 15.2843 2.71569 15.5903 3.09202 15.782C3.51984 16 4.07989 16 5.2 16Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </div>
       </div>
 
       {/* Button */}
-      <div className="text-center pt-2">
+      <div className="flex justify-center pt-5">
         <Button btnTxt={"Generate"} />
       </div>
     </div>
